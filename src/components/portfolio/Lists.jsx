e49186d3e65ddde9d6eef78d42f8e1520cc70ee8
jiyun03 <sprare06@gmail.com>
2023-03-28 17:30:55 +0900

    포트폴리오 리스트 sort 기능
    - sort 완료
    - 검색 완료
    - sort, 검색 연동 완료

--- src/components/portfolio/Lists.jsx
+++ src/components/portfolio/Lists.jsx
@@ -13,9 +13,6 @@ const path = process.env.PUBLIC_URL
 
 export default function Lists() {
   const defaultLimit = 6
-  const [lists, setLists] = useState([])
-  const [listsSort, setListsSort] = useState([])
-  const [listsLimit, setListsLimit] = useState(defaultLimit)
   const sortDefault = [
     {
       id: 'company',
@@ -30,20 +27,6 @@ export default function Lists() {
       item: [],
     },
   ]
-  const [sortArray, setSortArray] = useState(sortDefault)
-
-  // [검색] input에 작성한 타이틀, 서브타이틀 검색
-  const searchChange = (e) => {
-    const keyword = e.target.value.toLowerCase()
-    const search = lists.filter((list) => {
-      return (
-        list.title.toLowerCase().includes(keyword) ||
-        list.subtitle.toLowerCase().includes(keyword)
-      )
-    })
-    setListsSort(search)
-  }
-
   const sortContents = [
     {
       name: '회사별',
@@ -56,24 +39,98 @@ export default function Lists() {
     {
       name: '년도별',
       item: [
-        { id: 'year2023', name: '2023' },
-        { id: 'year2022', name: '2022' },
-        { id: 'year2021', name: '2021' },
+        { id: '2022', name: '2022' },
+        { id: '2021', name: '2021' },
+        { id: '2020', name: '2020' },
       ],
     },
     {
       name: '타입별',
       item: [
-        { id: 'type2023', name: '2023' },
-        { id: 'type2022', name: '2022' },
-        { id: 'type2021', name: '2021' },
+        { id: 'React', name: 'React' },
+        { id: 'SCSS', name: 'SCSS' },
+        { id: 'Firebase', name: 'Firebase' },
       ],
     },
   ]
+  // 전체 리스트 배열
+  const [lists, setLists] = useState([])
+  // sort 된 리스트 배열 (검색, sort)
+  const [listsSort, setListsSort] = useState([])
+  const [listsSortSearch, setListsSortSearch] = useState('')
+  // 리스트 limit 값 배열
+  const [listsLimit, setListsLimit] = useState(defaultLimit)
+  // sort 저장용 배열
+  const [sortArray, setSortArray] = useState(sortDefault)
+
+  // 검색어 state 저장
+  const searchChange = (e) => {
+    const keyword = e.target.value.toLowerCase()
+    setListsSortSearch(keyword)
+  }
 
   useEffect(() => {
-    console.log('change', sortArray)
-  }, [sortArray])
+    // sort 리스트
+    const sortLists = sortArray.reduce((acc, sort) => {
+      const item = sort.item
+      const filteredLists = lists.filter((list) => {
+        const date = list.date.split('.')[0]
+        const type = list.type.split('|')
+
+        return (
+          item.includes(list.company) ||
+          item.includes(date) ||
+          item.filter((x) => type.includes(x)).length !== 0
+        )
+      })
+      return [...acc, filteredLists]
+    }, [])
+
+    const sortFlat = sortLists.flat()
+
+    // sort id 별 개수 체크
+    const sortCountCheck = () => {
+      let count = 0
+      for (let index = 0; index < sortLists.length; index++) {
+        if (sortLists[index].length > 0) count++
+      }
+      return count
+    }
+
+    // [리팩토링] sort 중복 검수 리팩토링 필요
+    const sortDuplicatesFind =
+      sortCountCheck() > 1
+        ? sortFlat
+            .filter((item, index) => {
+              return sortFlat.indexOf(item) !== index
+            })
+            .sort((a, b) => new Date(b.date) - new Date(a.date))
+        : sortFlat
+
+    const sortDuplicates =
+      sortCountCheck() > 2
+        ? sortDuplicatesFind.filter((item, index) => {
+            return sortDuplicatesFind.indexOf(item) !== index
+          })
+        : sortDuplicatesFind
+
+    // [검색] input에 작성한 타이틀, 서브타이틀 검색
+    const search =
+      listsSortSearch !== ''
+        ? sortDuplicates.filter((list) => {
+            return (
+              list.title.toLowerCase().includes(listsSortSearch) ||
+              list.subtitle.toLowerCase().includes(listsSortSearch)
+            )
+          })
+        : sortDuplicates
+
+    if (sortFlat.length !== 0) {
+      setListsSort(search)
+    } else {
+      setListsSort(search)
+    }
+  }, [sortArray, lists, listsSortSearch])
 
   // [더보기] 더보기 버튼
   const listsMore = () => {
@@ -131,7 +188,7 @@ export default function Lists() {
       <ListsWrapper>
         {listsSort.length !== 0
           ? listsSort.map((item, idx) => {
-              if (idx > listsLimit - 1) return
+              if (idx > listsLimit - 1) return null
               return <ListsItem key={idx} item={item} />
             })
           : '결과가 없습니다'}
